b3fbd565b23162a500d588caeba6b09ac70f31f9
Unknown <davek032@outlook.com>
2019-09-29 17:55:46 +0100

    Some Basic Systems
    Created the core Gameobject for the player, with a temporary sprite. Player has controls that work with keyboard or controller
    Created an extremely basic test level with a rock to push out of the way - note: 'Arms out'  functionality isn't in yet

---
+++ PixelsFallFlat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 9.81f;
+
+    Rigidbody2D playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");        
+
+        Vector2 direction = new Vector2(h, v);
+
+        if (direction != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle * -1, Vector3.forward);
+        }
+
+        //transform.Translate(direction / 50);
+        playerBody.velocity = direction;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Movable")
+        {
+            collision.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            collision.gameObject.transform.parent.SetParent(this.transform);
+        }
+    }
+}

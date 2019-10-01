bae4fe739044e1d45a972feb7c3a52e195911bae
Szymon <wlodarczyk_szymon@wp.pl>
2019-10-01 15:01:23 +0100

    ITs WORKING

--- PixelsFallFlat/Assets/Scripts/RoomChange.cs
+++ PixelsFallFlat/Assets/Scripts/RoomChange.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class RoomChange : MonoBehaviour
 {
     public GameObject room;
+    public GameObject player1_pos;
+    public GameObject player2_pos;
     // Start is called before the first frame update
 
-        void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
         {
-        if (other.gameObject.name == "Player1" || other.gameObject.name == "Player2")
+        if (other.GetComponent<PlayerController>())
         {
             StartCoroutine(LoadRoom());
         }
@@ -20,6 +22,8 @@ public class RoomChange : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         GameObject.Find("Main Camera").transform.position = room.transform.position;
+        GameObject.Find("Player1").transform.position = player1_pos.transform.position;
+        GameObject.Find("Player2").transform.position = player2_pos.transform.position;
         //SceneManager.LoadScene(SceneName);
     }
 }

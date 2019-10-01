c859cc25b1dfac08a7825fe9f185aec5e44b271d
Szymon <wlodarczyk_szymon@wp.pl>
2019-10-01 14:24:07 +0100

    Camera position change instead of Scene

--- PixelsFallFlat/Assets/Scripts/RoomChange.cs
+++ PixelsFallFlat/Assets/Scripts/RoomChange.cs
@@ -5,20 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class RoomChange : MonoBehaviour
 {
-    public string SceneName;
+    public GameObject room;
     // Start is called before the first frame update
 
         void OnTriggerEnter2D(Collider2D other)
         {
-        if (other.CompareTag("Movable"))
+        if (other.gameObject.name == "Player1" || other.gameObject.name == "Player2")
         {
-            StartCoroutine(LoadScene());
+            StartCoroutine(LoadRoom());
         }
         }
 
-    IEnumerator LoadScene()
+    IEnumerator LoadRoom()
     {
         yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadScene(SceneName);
+        GameObject.Find("Main Camera").transform.position = room.transform.position;
+        //SceneManager.LoadScene(SceneName);
     }
 }

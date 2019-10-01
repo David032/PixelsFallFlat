6dee46d9edfaff4d0bf35ac0605fa7e204dd5ec6
Brendon Thompson <Brendon2.Thompson@live.uwe.ac.uk>
2019-10-01 10:42:55 +0100

    Basic Object-Object interaction scripts for our special intractable objects in game

---
+++ PixelsFallFlat/Assets/Scripts/AxeWTree.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeWTree : MonoBehaviour
+{
+    public Transform spawnPos;
+
+    public GameObject tree;
+    public GameObject axe;
+    public GameObject trunk;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == axe)
+        {
+            //Destroy(tree.gameObject);
+            //Destroy(this.gameObject);
+            //Instantiate(trunk, spawnPos.position, spawnPos.rotation);
+        }
+    }
+}
---
+++ PixelsFallFlat/Assets/Scripts/HammerWBoulder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerWBoulder : MonoBehaviour
+{
+    public Transform spawnPos;
+
+    public GameObject boulder;
+    public GameObject hammer;
+    public GameObject rock1;
+    public GameObject rock2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == hammer)
+        {
+            //Destroy(boulder.gameObject);
+            //Destroy(this.gameObject);
+            //Instantiate(rock1, spawnPos.position, spawnPos.rotation);
+            //Instantiate(rock2, spawnPos.position, spawnPos.rotation);
+        }
+    }
+}

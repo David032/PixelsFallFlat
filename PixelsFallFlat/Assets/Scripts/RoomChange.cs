a2de94aa47234f541d9abfbf681a64eb66af1dcc
Szymon <wlodarczyk_szymon@wp.pl>
2019-10-01 01:29:56 +0100

    Basic Scene Change

---
+++ PixelsFallFlat/Assets/Scripts/RoomChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomChange : MonoBehaviour
+{
+    public string SceneName;
+    // Start is called before the first frame update
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+        if (other.CompareTag("Movable"))
+        {
+            StartCoroutine(LoadScene());
+        }
+        }
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneName);
+    }
+}

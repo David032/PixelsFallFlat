ad1e1af434d0157b20a3cbcc10d3de831014618d
David Kelly <davek032@outlook.com>
2019-10-06 16:12:34 +0100

    Bug Fixes Galore
    Player rotation is now snappier
    Physics interactions should now not push the player in the opposite direction to the intended - This was caused by changing the box collider when grabbing something
    Multiplayer is now back in - transitions between the rooms in level one are updated, haven't done the other as I can't see the door spots?
    Axes are mostly rebound - Player 1 Vertical isn't though :(

--- PixelsFallFlat/Assets/Scripts/PlayerController.cs
+++ PixelsFallFlat/Assets/Scripts/PlayerController.cs
@@ -19,8 +19,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
     public Vector2 playerVelocity;
 
-    string hAxis = "Horizontal";
-    string vAxis = "Vertical";
+    string hAxis = "P1Horizontal";
+    string vAxis = "P1V";
 
     bool armsOut = false;
     GameObject grabbed = null;
@@ -51,15 +51,20 @@ public class PlayerController : MonoBehaviour
         speed = pWeight / GetComponent<Rigidbody2D>().mass;
 
         Vector2 direction = new Vector2(h, v);
+        print(h + v);       
+
 
         if (direction != Vector2.zero) ///Add '&& grabbed == null' here to disable rotation whilst holding an object, trying to work out how to give better rotation
         {
             float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle * -1, Vector3.forward);
+            //transform.rotation = Quaternion.AngleAxis(angle * -1, Vector3.forward); - Old rotation, might not be as snappy
+            transform.rotation.eulerAngles.Set(0, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 0, angle * -1);
         }
 
         playerVelocity = direction.normalized * speed;
-        playerBody.MovePosition(playerBody.position + playerVelocity * Time.fixedDeltaTime);
+        Vector2 pos = playerBody.position + playerVelocity * Time.fixedDeltaTime;
+        playerBody.MovePosition(pos);
     }
 
     void Update()
@@ -69,14 +74,14 @@ public class PlayerController : MonoBehaviour
         if (armsOut)
         {
             GetComponent<CapsuleCollider2D>().enabled = true;
-            GetComponent<BoxCollider2D>().offset = new Vector2(0,-0.005f);
-            GetComponent<BoxCollider2D>().size = new Vector2(0.14f, 0.13f);
+            //GetComponent<BoxCollider2D>().offset = new Vector2(0,-0.005f);
+            //GetComponent<BoxCollider2D>().size = new Vector2(0.14f, 0.13f);
         }
         else if (!armsOut)
         {
             GetComponent<CapsuleCollider2D>().enabled = false;
-            GetComponent<BoxCollider2D>().offset = new Vector2(0f,-0.038f);
-            GetComponent<BoxCollider2D>().size = new Vector2(0.14f,0.065f);
+            //GetComponent<BoxCollider2D>().offset = new Vector2(0f,-0.038f);
+            //GetComponent<BoxCollider2D>().size = new Vector2(0.14f,0.065f);
         }
     }
 
@@ -85,12 +90,12 @@ public class PlayerController : MonoBehaviour
         switch (thisPlayer)
         {
             case Player.Player1:
-                if (Input.GetButtonDown("Fire1"))
+                if (Input.GetButtonDown("P1Grab"))
                 {
                     GetComponent<SpriteRenderer>().sprite = armedPlayer;
                     armsOut = true;
                 }
-                else if (Input.GetButtonUp("Fire1"))
+                else if (Input.GetButtonUp("P1Grab"))
                 {
                     GetComponent<SpriteRenderer>().sprite = basePlayer;
                     armsOut = false;
@@ -145,10 +150,13 @@ public class PlayerController : MonoBehaviour
                 GetComponent<Rigidbody2D>().mass += collision.GetComponent<Rigidbody2D>().mass;
                 grabbedPosition = grabbed.GetComponent<Rigidbody2D>().position;
 
-                grabbed.GetComponent<Rigidbody2D>().useFullKinematicContacts = true;
                 grabbed.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                grabbed.GetComponent<Rigidbody2D>().useFullKinematicContacts = true;
                 grabbed.GetComponent<Rigidbody2D>().velocity.Set(0, 0);
                 grabbed.GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+                GetComponent<Rigidbody2D>().velocity.Set(0, 0);
+                GetComponent<Rigidbody2D>().angularVelocity = 0;
             }
         }
 
--- PixelsFallFlat/Assets/Scripts/RoomChange.cs
+++ PixelsFallFlat/Assets/Scripts/RoomChange.cs
@@ -7,8 +7,7 @@ public class RoomChange : MonoBehaviour
 {
     public GameObject room;
     public GameObject player1_pos;
-    //public GameObject player2_pos;
-    // Start is called before the first frame update
+    public GameObject player2_pos;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -22,8 +21,8 @@ public class RoomChange : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         GameObject.Find("Main Camera").transform.position = room.transform.position;
-        GameObject.Find("man1").transform.position = player1_pos.transform.position;
-      // GameObject.Find("man2").transform.position = player2_pos.transform.position;
+        GameObject.Find("Player1").transform.position = player1_pos.transform.position;
+        GameObject.Find("Player2").transform.position = player2_pos.transform.position;
         //SceneManager.LoadScene(SceneName);
     }
 }

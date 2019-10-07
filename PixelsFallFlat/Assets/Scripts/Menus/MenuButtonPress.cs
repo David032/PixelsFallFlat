fdd2709ecdb82e8b9c64fd78d5653a2fae193e7e
Unknown <pokepeel15@yahoo.co.uk>
2019-10-08 00:58:39 +0100

    Added Level select script and WIP sound script
    Altered the menu BG to represent the new screen size, added level select menu (Needs finalising due to PC errors), added a WIP script to play menu sounds if needed.

---
+++ PixelsFallFlat/Assets/Scripts/Menus/MenuButtonPress.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonPress : MonoBehaviour
+{
+    public AudioClip menu_move;
+    public AudioSource audio_source;
+
+    bool move = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move = Input.GetButtonDown("P1Vertical");
+        PlaySound();
+    }
+
+    private void PlaySound()
+    {
+        audio_source.clip = menu_move;
+        audio_source.Play();
+    }
+}

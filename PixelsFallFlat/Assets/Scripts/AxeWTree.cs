c46937eab8f9694cdbc70b17bf58d7f1a9ac2875
kai tooley <kai.p.o.tooley@gmail.com>
2019-10-08 11:29:46 +0100

    breakable wall and tree disable void

--- PixelsFallFlat/Assets/Scripts/AxeWTree.cs
+++ PixelsFallFlat/Assets/Scripts/AxeWTree.cs
@@ -8,6 +8,8 @@ public class AxeWTree : MonoBehaviour
     public GameObject tree;
     public GameObject axe;
     public GameObject trunk;
+    public GameObject[] voidTiles;
+
 
     void FixedUpdate()
     {
@@ -19,6 +21,10 @@ public class AxeWTree : MonoBehaviour
         {
             Debug.Log("Stuff");
             Instantiate(trunk, transform.position + new Vector3(0.3f, 0f, 0f), new Quaternion());
+            foreach(GameObject tile in voidTiles)
+            {
+                tile.GetComponent<BoxCollider2D>().enabled = false;
+            }
             Destroy(this.gameObject);
         }
         Debug.Log("Other Stuff");

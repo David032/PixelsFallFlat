78cb6dae352ddee639dea754ebd9e98ecc6ee261
Kai-Tooley <kai.p.o.tooley@gmail.com>
2019-10-15 17:32:52 +0100

    modify door activation checks to work with array of items

--- PixelsFallFlat/Assets/Scripts/OpenKeyDoor.cs
+++ PixelsFallFlat/Assets/Scripts/OpenKeyDoor.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class OpenKeyDoor : MonoBehaviour
 {
-    public bool isDoorOpen;
-    public GameObject door;
+    public GameObject[] keyLocks;
+
+    bool isDoorOpen;
     public Sprite openedDoor;
-
-
 
     // Update is called once per frame
     void Update()
     {
-        isDoorOpen = GetComponent<LockWKey>().doorIsOpen;
+        isDoorOpen = true;
+        foreach (var keyLock in keyLocks)
+        {
+            if (!keyLock.GetComponent<LockWKey>().doorIsOpen)
+            {
+                isDoorOpen = false;
+                break;
+            }
+        }
+
          if(isDoorOpen)
         {
-            door.GetComponent<RoomChange>().canMove = true;
-            door.GetComponent<SpriteRenderer>().sprite = openedDoor;
+            if (GetComponent<RoomChange>() != null)
+            {
+                GetComponent<RoomChange>().canMove = true;
+            }
+            else
+            {
+                GetComponent<BoxCollider2D>().enabled = false;
+            }
+            GetComponent<SpriteRenderer>().sprite = openedDoor;
         }
     }
 }
